b37d0fd61633a6fef6a87e2daa05053ec8e41f6f
guiioshua <guilhermeioshua@gmail.com>
2026-01-15 17:58:11 -0300

    Setup inicial do Docker, containerização do app de dados mockados. API funcional

---
+++ source_server/main.py
@@ -0,0 +1,26 @@
+import os
+from flask import Flask, jsonify
+import pandas as pd
+
+app = Flask(__name__)
+
+DADOS_CSV = 'orders.csv'
+
+@app.route('/transactions', methods=['GET'])
+def get_transactions():
+    if not os.path.exists(DADOS_CSV):
+        return jsonify({"error": "CSV não foi achado"})
+    try:
+        df = pd.read_csv(DADOS_CSV, sep=';')
+        if df['value'].dtype == 'object':
+            df['value'].str.replace(',', '.').astype(float)
+        
+        dados = df.to_dict(orient='records')
+        return jsonify(dados)
+    
+    except Exception as error:
+        return jsonify({"error": str(error)}), 500
+        
+    
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8000)
