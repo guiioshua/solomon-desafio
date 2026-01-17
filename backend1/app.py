9dce86aa0051ff69d065148023d5a00906c39836
guiioshua <guilhermeioshua@gmail.com>
2026-01-17 01:20:45 -0300

    core do servidor do backend1, com rota para login funcional para autenticação

---
+++ backend1/app.py
@@ -0,0 +1,67 @@
+from datetime import timedelta
+from dotenv import load_dotenv
+from werkzeug.security import check_password_hash
+from os import getenv, environ
+import psycopg2
+
+from flask import Flask
+from flask import jsonify
+from flask import request
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
+
+load_dotenv()
+
+app = Flask(__name__)
+
+app.config["JWT_SECRET_KEY"] = environ["API_SECRET_KEY"]
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(minutes=30)
+jwt = JWTManager(app)
+
+DB_URL = getenv("DATABASE_URL")
+connection = psycopg2.connect(DB_URL)
+
+@app.route("/login", methods=["POST"])
+def login():
+    # Já checa content type
+    data = request.get_json()
+    email_input = data.get("email")
+    password_input = data.get("password")
+
+    if not email_input or not password_input:
+        return jsonify({"msg": "Credenciais ausentes"}), 400
+
+    with connection.cursor() as cur:
+        query = "SELECT id, email, password_hash FROM auth.users WHERE email = %s"
+        cur.execute(query, (email_input,))
+        record = cur.fetchone()
+        
+        if not record:
+            # Pode ser genérico também
+            return jsonify({"msg": "Email não existe"}), 401
+
+        id_db, email_db, password_hash_db = record 
+        
+        if not check_password_hash(password_hash_db, password_input):
+            return jsonify({"msg": "Senha incorreta"}), 401    
+        
+        # Create token with the user ID as identity
+        access_token = create_access_token(identity=str(id_db))
+          
+    return jsonify(access_token=access_token), 200
+            
+        
+# Protect a route with jwt_required, which will kick out requests
+# without a valid JWT present.
+@app.route("/protected", methods=["GET"])
+@jwt_required()
+def protected():
+    # Access the identity of the current user with get_jwt_identity
+    current_user = get_jwt_identity()
+    return jsonify(logged_in_as=current_user), 200
+
+
+if __name__ == "__main__":
+    app.run()
